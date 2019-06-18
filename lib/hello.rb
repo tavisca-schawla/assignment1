da1931204a27c8a07994d66948d8f77f80ae1380
Sundesh Mahesh Chawla <sundesh.chawla@gmail.com>
2019-06-18 16:15:37 +0530

    Hello uses greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,7 @@
-# This is a bad comment, we want to revert it.
-# Author: Sundesh Chawla (sundesh.chawla@gmail.com)
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
-puts "Hello, #{name}!"
+
+greeter = Greeter.new(name)
+puts greeter.greet
